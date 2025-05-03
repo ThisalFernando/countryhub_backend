8943d69b3141183010311e3773fcd65d2214af8e
thisal <thisalfernando39@gmail.com>
2025-05-03 22:19:10 +0530

    Rest countries server side

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from "express"
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoutes.js";
+import favoriteRoutes from "./routes/favoriteRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors({
+    origin: "https://countryhub-frontend.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+app.use(helmet());
+app.use(morgan("dev"));
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/favorites", favoriteRoutes);
+
+
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
